51251070a6f4b531ea06a748c88f80508f528733
Michael Dyrynda <michael@dyrynda.com.au>
2024-10-13 22:11:54 +1030

    remove alias

--- src/Resources/Test.php
+++ src/Resources/Test.php
@@ -4,22 +4,23 @@ declare(strict_types=1);
 
 namespace Dyrynda\Stagetimer\Resources;
 
-use Dyrynda\Stagetimer\Data\Test as Data;
+use Dyrynda\Stagetimer\Data\Test\AuthorizationData;
+use Dyrynda\Stagetimer\Data\Test\ConnectionData;
 use Dyrynda\Stagetimer\Requests\Test as Requests;
 use Dyrynda\Stagetimer\Resource;
 
 final class Test extends Resource
 {
-    public function connection(): Data\ConnectionData
+    public function connection(): ConnectionData
     {
-        return Data\ConnectionData::fromArray(
+        return ConnectionData::fromArray(
             $this->connector->send(new Requests\Connection)->json()
         );
     }
 
-    public function authorization(string $roomId): Data\AuthorizationData
+    public function authorization(string $roomId): AuthorizationData
     {
-        return Data\AuthorizationData::fromArray(
+        return AuthorizationData::fromArray(
             $this->connector->send(new Requests\Authorization($roomId))->json()
         );
     }
