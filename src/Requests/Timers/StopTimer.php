15e94ed2253c06b6fe50b8bb7b30a7f1ff306111
Michael Dyrynda <michael@dyrynda.com.au>
2024-10-25 22:44:41 +1030

    add ability to pass index to timer endpoints

--- src/Requests/Timers/StopTimer.php
+++ src/Requests/Timers/StopTimer.php
@@ -12,6 +12,7 @@ class StopTimer extends Request
     public function __construct(
         #[SensitiveParameter] private string $roomId,
         #[SensitiveParameter] private string $timerId,
+        private ?int $index = null,
     ) {}
 
     public function resolveEndpoint(): string
@@ -21,9 +22,10 @@ class StopTimer extends Request
 
     protected function defaultQuery(): array
     {
-        return [
+        return array_filter([
             'room_id' => $this->roomId,
             'timer_id' => $this->timerId,
-        ];
+            'index' => $this->index,
+        ]);
     }
 }
