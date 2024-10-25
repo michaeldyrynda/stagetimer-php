180f3c68ff25bd65d83494f6d2dbb5d8bcb2573b
Michael Dyrynda <michael@dyrynda.com.au>
2024-10-25 22:47:44 +1030

    add todo tests for message endpoints

---
+++ tests/Feature/MessageEndpointsTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Message endpoints', function () {
+    it('can create a message', function () {})->todo();
+
+    it('can update a message', function () {})->todo();
+
+    it('can show a message', function () {})->todo();
+
+    it('can hide a message', function () {})->todo();
+
+    it('can toggle a message', function () {})->todo();
+
+    it('can get all message', function () {})->todo();
+
+    it('can get a message', function () {})->todo();
+
+    it('can delete a message', function () {})->todo();
+});
